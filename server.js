6373a2f9ffc74c3685eb48d43780a22c8e3e2504
Alii-s <119129254+Alii-s@users.noreply.github.com>
2024-05-14 22:09:21 +0300

    Synchronizing server

--- server.js
+++ server.js
@@ -1,60 +1,67 @@
 const axios = require("axios");
-//const { default: Quill } = require("quill");
-//var quill = new Quill;
-//const delta = Quill.import("delta");
 const io = require("socket.io")(3001, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
+const AsyncLock = require('async-lock');
+const lock = new AsyncLock();
+
 let serverVersion = 0;
 let documentsOperations = new Map(); // {documentId: [operations]}
+
 io.on("connection", (socket) => {
   socket.on("get-document", async (documentId, userId) => {
     const document = await findOrCreateDocument(documentId, userId);
     console.log("got this document:", document);
     socket.join(documentId);
     socket.emit("load-document", document, serverVersion);
+
     if (!documentsOperations.has(documentId)) {
-      //if the document is not in the map create a new entry
+      // If the document is not in the map, create a new entry
       documentsOperations.set(documentId, []);
     }
-    socket.on("send-changes", (delta, clientVersion, acknowledgeID) => {
+
+    socket.on("send-changes", async (delta, clientVersion, acknowledgeID) => {
       console.log("I am in send-changes");
-      let operations = documentsOperations.get(documentId); //get the operations for the document
-      serverVersion = operations.length;
-      if (serverVersion !== 0)
-        delta = operationalTransform(
-          delta,
-          operations,
-          clientVersion,
-          serverVersion
-        );
-      let modifiedDelta = delta;
-      if (modifiedDelta.ops[0] && !("retain" in modifiedDelta.ops[0])) {
-        modifiedDelta.ops.unshift({ retain: 0 });
-      }
-      operations.push(modifiedDelta);
-      let tempV = serverVersion + 1;
-      if (delta.ops[0] && delta.ops[0].retain == 0) {
-        delta.ops = [delta.ops[1]];
-      }
-      console.log(tempV, delta);
-      socket.broadcast
-        .to(documentId)
-        .emit("receive-changes", delta, tempV, acknowledgeID); // QUESTION: should we increment the server version before or after broadcasting the changes?
-      socket.emit("acknowledge", acknowledgeID, tempV);
+
+      await lock.acquire(documentId, async () => {
+        let operations = documentsOperations.get(documentId); // Get the operations for the document
+        serverVersion = operations.length;
+
+        if (serverVersion !== 0) {
+          delta = operationalTransform(delta, operations, clientVersion, serverVersion);
+        }
+
+        let modifiedDelta = delta;
+        if (modifiedDelta.ops[0] && !("retain" in modifiedDelta.ops[0])) {
+          modifiedDelta.ops.unshift({ retain: 0 });
+        }
+
+        operations.push(modifiedDelta);
+        let tempV = serverVersion + 1;
+
+        if (delta.ops[0] && delta.ops[0].retain == 0) {
+          delta.ops = [delta.ops[1]];
+        }
+
+        console.log(tempV, delta);
+        socket.broadcast
+          .to(documentId)
+          .emit("receive-changes", delta, tempV, acknowledgeID); // Broadcast changes
+
+        socket.emit("acknowledge", acknowledgeID, tempV);
+      });
     });
 
     socket.on("cursor-change", (cursor) => {
-      console.log("Cursor change event received:", cursor);
+      //console.log("Cursor change event received:", cursor);
       // Broadcast the cursor change to all other clients except the sender
       socket.broadcast.emit("cursor-change", cursor);
     });
 
     socket.on("save-document", async (data) => {
-      // console.log("I am being called");
       await findByIdAndUpdate(documentId, userId, { data });
     });
   });
@@ -67,14 +74,13 @@ async function findOrCreateDocument(fileId, userId) {
     `http://localhost:8081/file/${fileId}/${userId}`
   );
   if (result.data) {
-    //console.log(result.data);
     return result.data.fileContent;
   }
 }
 
 async function findByIdAndUpdate(documentId, userId, { data }) {
   if (documentId == null || userId == null || data == null) return null;
-  //console.log("my data is", data);
+
   const result = await axios.patch(
     `http://localhost:8081/file/saveEdits/${documentId}/${userId}`,
     {
@@ -83,7 +89,6 @@ async function findByIdAndUpdate(documentId, userId, { data }) {
   );
 
   if (result.data) {
-    // console.log(result.data);
     return result.data.fileContent;
   }
 }
@@ -96,11 +101,11 @@ function operationalTransform(delta, operations, clientVersion, serverVersion) {
     serverVersion
   );
 
-  let varIdx = 1;
   if (delta.ops[0] && !("retain" in delta.ops[0])) {
     delta.ops.unshift({ retain: 0 });
-    console.log("unsaewaewadsshifting delta:", delta);
+    console.log("Unshifting delta:", delta);
   }
+
   if (clientVersion >= serverVersion) {
     if (delta.ops[0] && delta.ops[0].retain == 0) {
       delta.ops = [delta.ops[1]];
@@ -108,28 +113,21 @@ function operationalTransform(delta, operations, clientVersion, serverVersion) {
     console.log("I am returning delta", delta);
     return delta;
   }
+
   for (let i = clientVersion; i < serverVersion; i++) {
     if (operations[i].ops[0] && !("retain" in operations[i].ops[0])) {
       operations[i].ops.unshift({ retain: 0 });
     }
+
     console.log("I survived", i, "times");
     console.log(delta.ops, "delta.ops");
     console.log(operations[i], "operations[i]");
+
     if ("insert" in delta.ops[1]) {
       if ("insert" in operations[i].ops[1]) {
         if (delta.ops[0].retain >= operations[i].ops[0].retain) {
           console.log("I am insert insert", i);
-          console.log(delta.ops[0].retain, "Retain1 in insert insert");
-          console.log(
-            operations[i].ops[1].insert.length,
-            "Retain2 in insert insert"
-          );
-          console.log(
-            delta.ops[0].retain + operations[i].ops[1].insert.length,
-            "Retain3 in insert insert"
-          );
-          delta.ops[0].retain += operations[i].ops[1].insert.length;
-          console.log(delta.ops[0].retain, "Retain4 in insert insert");
+          delta.ops[0].retain += operations[i].ops[1].insert.length - 1;
         }
       } else if ("delete" in operations[i].ops[1]) {
         if (delta.ops[0].retain >= operations[i].ops[0].retain) {
@@ -140,7 +138,7 @@ function operationalTransform(delta, operations, clientVersion, serverVersion) {
     } else if ("delete" in delta.ops[1]) {
       if ("insert" in operations[i].ops[1]) {
         if (delta.ops[0].retain >= operations[i].ops[0].retain) {
-          delta.ops[0].retain += operations[i].ops[1].insert.length;
+          delta.ops[0].retain += operations[i].ops[1].insert.length - 1;
           console.log("I am delete insert");
         }
       } else if ("delete" in operations[i].ops[1]) {
@@ -151,9 +149,11 @@ function operationalTransform(delta, operations, clientVersion, serverVersion) {
       }
     }
   }
+
   if (delta.ops[0] && delta.ops[0].retain == 0) {
     delta.ops = [delta.ops[1]];
   }
+
   console.log("I am returning delta", delta);
   return delta;
 }
